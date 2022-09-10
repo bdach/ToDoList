0400486b341c5770fe6dbadd48a79931fe29f581
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2022-09-11 00:34:07 +0200

    Set up structure for today page

---
+++ Ticketron.App/Views/Tasks/TodayPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Ticketron.App.Views.Tasks
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TodayPage : Page
+    {
+        public TodayPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
